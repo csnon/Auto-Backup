8658bb37224224b30b92d75f2865db4be2b67d1e
Csnon <72682205+csnon@users.noreply.github.com>
2022-03-16 16:05:57 +0700

    Add files via upload

---
+++ Non-Backup/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Non_Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(359, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(73, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(359, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(73, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(359, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Webhook :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File 3 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "File 2 :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "File 1 :";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(73, 136);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(351, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Timer :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(425, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "0 Hour";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 33);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(293, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 33);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(438, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(36, 19);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(438, 87);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(36, 19);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(438, 112);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(36, 19);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(176, 183);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Auto Backup = OFF";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(481, 218);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Backup By Non-Dev";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Non-Backup/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+namespace Non_Backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Anonfiles.com upload
+        static string CreateDownloadLink(string File)
+        {
+            string ReturnValue = string.Empty;
+            try
+            {
+                using (WebClient Client = new WebClient())
+                {
+                    byte[] Response = Client.UploadFile("https://api.anonfiles.com/upload", File); //Upload And Return Response
+                    string ResponseBody = Encoding.ASCII.GetString(Response);
+                    if (ResponseBody.Contains("\"error\": {")) // Simple Error Handling
+                    {
+                        ReturnValue += "There was a erorr while uploading the file.\r\n";
+                        ReturnValue += "Error message: " + ResponseBody.Split('"')[7] + "\r\n";
+                    }
+                    else
+                    {
+                        ReturnValue += ResponseBody.Split('"')[15] + "\r\n"; // 
+                    }
+                }
+            }
+            catch (Exception Exception)
+            {
+                ReturnValue += "Exception Message:\r\n" + Exception.Message + "\r\n"; // Return Error 
+            }
+            return ReturnValue;
+        }
+        //Webhooks
+        private static string defaultUserAgent = "Elysia#0896";
+        private static string defaultAvatar = "https://cdn.discordapp.com/attachments/798782780292333571/886640991308746862/20210826_103405.jpg";
+
+        public static string Send(string mssgBody, string userName, string webhook)
+        {
+            // Generate post objects
+            Dictionary<string, object> postParameters = new Dictionary<string, object>();
+            postParameters.Add("username", userName);
+            postParameters.Add("content", mssgBody);
+            postParameters.Add("avatar_url", defaultAvatar);
+
+            // Create request and receive response
+            HttpWebResponse webResponse = FormUpload.MultipartFormDataPost(webhook, defaultUserAgent, postParameters);
+
+            // Process response
+            StreamReader responseReader = new StreamReader(webResponse.GetResponseStream());
+            string fullResponse = responseReader.ReadToEnd();
+            webResponse.Close();
+
+
+            //return string with response
+            return fullResponse;
+        }
+
+        public static string SendFile(
+            string mssgBody,
+            string filename,
+            string fileformat,
+            string filepath,
+            string application,
+            string userName,
+            string webhooklink)
+        {
+            // Read file data
+            FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read);
+            byte[] data = new byte[fs.Length];
+            fs.Read(data, 0, data.Length);
+            fs.Close();
+
+            // Generate post objects
+            Dictionary<string, object> postParameters = new Dictionary<string, object>();
+            postParameters.Add("filename", filename);
+            postParameters.Add("fileformat", fileformat);
+            postParameters.Add("file", new FormUpload.FileParameter(data, filename, application/*"application/msexcel"*/));
+
+            postParameters.Add("username", userName);
+            postParameters.Add("content", mssgBody);
+            postParameters.Add("avatar_url", defaultAvatar);
+
+            // Create request and receive response
+            HttpWebResponse webResponse = FormUpload.MultipartFormDataPost(webhooklink, defaultUserAgent, postParameters);
+
+            // Process response
+            StreamReader responseReader = new StreamReader(webResponse.GetResponseStream());
+            string fullResponse = responseReader.ReadToEnd();
+            webResponse.Close();
+
+            //return string with response
+            return fullResponse;
+        }
+
+        public static class FormUpload //formats data as a multi part form to allow for file sharing
+        {
+            private static readonly Encoding encoding = Encoding.UTF8;
+            public static HttpWebResponse MultipartFormDataPost(string postUrl, string userAgent, Dictionary<string, object> postParameters)
+            {
+                string formDataBoundary = String.Format("----------{0:N}", Guid.NewGuid());
+
+                string contentType = "multipart/form-data; boundary=" + formDataBoundary;
+
+                byte[] formData = GetMultipartFormData(postParameters, formDataBoundary);
+
+                return PostForm(postUrl, userAgent, contentType, formData);
+            }
+
+            private static HttpWebResponse PostForm(string postUrl, string userAgent, string contentType, byte[] formData)
+            {
+                HttpWebRequest request = WebRequest.Create(postUrl) as HttpWebRequest;
+
+                if (request == null)
+                {
+
+                    throw new NullReferenceException("request is not a http request");
+                }
+
+                // Set up the request properties.
+                request.Method = "POST";
+                request.ContentType = contentType;
+                request.UserAgent = userAgent;
+                request.CookieContainer = new CookieContainer();
+                request.ContentLength = formData.Length;
+
+                // Send the form data to the request.
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    requestStream.Write(formData, 0, formData.Length);
+                    requestStream.Close();
+                }
+
+                return request.GetResponse() as HttpWebResponse;
+            }
+
+            private static byte[] GetMultipartFormData(Dictionary<string, object> postParameters, string boundary)
+            {
+                Stream formDataStream = new System.IO.MemoryStream();
+                bool needsCLRF = false;
+
+                foreach (var param in postParameters)
+                {
+                    if (needsCLRF)
+                        formDataStream.Write(encoding.GetBytes("\r\n"), 0, encoding.GetByteCount("\r\n"));
+
+                    needsCLRF = true;
+
+                    if (param.Value is FileParameter)
+                    {
+                        FileParameter fileToUpload = (FileParameter)param.Value;
+
+                        string header = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"; filename=\"{2}\"\r\nContent-Type: {3}\r\n\r\n",
+                            boundary,
+                            param.Key,
+                            fileToUpload.FileName ?? param.Key,
+                            fileToUpload.ContentType ?? "application/octet-stream");
+
+                        formDataStream.Write(encoding.GetBytes(header), 0, encoding.GetByteCount(header));
+
+                        formDataStream.Write(fileToUpload.File, 0, fileToUpload.File.Length);
+                    }
+                    else
+                    {
+                        string postData = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"\r\n\r\n{2}",
+                            boundary,
+                            param.Key,
+                            param.Value);
+                        formDataStream.Write(encoding.GetBytes(postData), 0, encoding.GetByteCount(postData));
+                    }
+                }
+
+                // Add the end of the request.  Start with a newline
+                string footer = "\r\n--" + boundary + "--\r\n";
+                formDataStream.Write(encoding.GetBytes(footer), 0, encoding.GetByteCount(footer));
+
+                // Dump the Stream into a byte[]
+                formDataStream.Position = 0;
+                byte[] formData = new byte[formDataStream.Length];
+                formDataStream.Read(formData, 0, formData.Length);
+                formDataStream.Close();
+
+                return formData;
+            }
+
+            public class FileParameter
+            {
+                public byte[] File { get; set; }
+                public string FileName { get; set; }
+                public string ContentType { get; set; }
+                public FileParameter(byte[] file) : this(file, null) { }
+                public FileParameter(byte[] file, string filename) : this(file, filename, null) { }
+                public FileParameter(byte[] file, string filename, string contenttype)
+                {
+                    File = file;
+                    FileName = filename;
+                    ContentType = contenttype;
+                }
+            }
+        }
+        //timer scroll bar
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label6.Text = trackBar1.Value.ToString() + " Hour";
+        }
+        //select your file 1
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "File 1";
+            if (fbd.ShowDialog() == DialogResult.OK)
+                textBox2.Text = fbd.SelectedPath;
+        }
+        //select your file 2
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "File 2";
+            if (fbd.ShowDialog() == DialogResult.OK)
+                textBox3.Text = fbd.SelectedPath;
+        }
+        //select your file 3
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "File 3";
+            if (fbd.ShowDialog() == DialogResult.OK)
+                textBox4.Text = fbd.SelectedPath;
+        }
+        //start button
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please Input The Webhook Link", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Please Choose the file ", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }// 1 hour = 3600000 ms
+            if (trackBar1.Value < 1)
+            {
+                MessageBox.Show("Please Choose the timer value", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                button1.Enabled = false;
+                int BackupTimer = trackBar1.Value * 60000;
+                timer1.Interval = BackupTimer;
+                timer1.Start();
+                label7.Text = "Auto Backup = ON";
+            }
+
+        }
+        //stop button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            timer1.Stop();
+            label7.Text = "Auto Backup = OFF";
+            if (backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string desktop = Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
+            string file1 = @textBox2.Text;
+            string file2 = @textBox3.Text;
+            string file3 = @textBox4.Text;
+            string zipfile1 = desktop + @"\\file1.zip";
+            string zipfile2 = desktop + @"\\file2.zip";
+            string zipfile3 = desktop + @"\\file3.zip";
+            if (File.Exists(zipfile1))
+            {
+                File.Delete(zipfile1);
+            }
+            if (File.Exists(zipfile2))
+            {
+                File.Delete(zipfile2);
+            }
+            if (File.Exists(zipfile3))
+            {
+                File.Delete(zipfile3);
+            }
+            if (!string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                ZipFile.CreateFromDirectory(file1, desktop + @"\\file1.zip");
+                if (File.Exists(zipfile1))
+                {
+                    String Link1 = CreateDownloadLink(zipfile1);
+                    Send("**File 1 : " + Link1, "Non-Backup | Auto backup", textBox1.Text);
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                ZipFile.CreateFromDirectory(file2, desktop + @"\\file2.zip");
+                if (File.Exists(zipfile2))
+                {
+                    String Link2 = CreateDownloadLink(zipfile2);
+                    Send("**File 2 : " + Link2, "Non-Backup | Auto backup", textBox1.Text);
+                }
+            }
+            if (!string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                ZipFile.CreateFromDirectory(file3, desktop + @"\\file3.zip");
+                if (File.Exists(zipfile3))
+                {
+                    String Link3 = CreateDownloadLink(zipfile3);
+                    Send("**File 3 : " + Link3, "Non-Backup | Auto backup", textBox1.Text);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
